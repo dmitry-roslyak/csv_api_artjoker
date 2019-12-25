6847229033c908636bc6a73d9236ff1d5b355b81
Dmitry Roslyak <dmitri.roslyak@gmail.com>
2019-12-25 14:01:06 +0200

    users get csv

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const User = require("../models").User;
+const csv = require('csv');
 
-router.get('/json', function (req, res, next) {
+const usersHeaders = ["UserName", "FirstName", "LastName", "Age"];
+
+router.get('/', function (req, res, next) {
   User.findAll().then(users => {
     res.send(users)
+  });
+});
+
+router.get('/csv', function (req, res, next) {
+  res.setHeader('Content-disposition', 'attachment; filename=users.csv');
+  res.set('Content-Type', 'text/csv');
+
+  User.findAll().then(users => {
+    csv.stringify(users, {
+      header: true,
+      columns: usersHeaders
+    }).pipe(res);
   });
 });
 
